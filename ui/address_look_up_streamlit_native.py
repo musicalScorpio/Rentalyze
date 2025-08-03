bf7b6f8f8b2683910ab205806d6f28ca4a9872b1
musicalscorpio <smukherjee0891@gmail.com>
2025-08-03 16:42:31 -0400

    exposed the logic through the stremlit native to bypass APIS for now

--- ui/address_look_up_streamlit_native.py
+++ ui/address_look_up_streamlit_native.py
@@ -74,7 +74,7 @@ def main():
                     if offer_price and rent_price:
                         #url = f"{ADDRESS_LOOKUP_API_URL}/roi-estimate?price={offer_price}&rent={rent_price}&state={location['state_code']}&down_payment={.25 * offer_price}&credit_score={700}&has_mortgage=True"
                         #response = requests.get(url)
-                        resp_json = address_lookup_service.roi_estimate(price=offer_price,rent=rent_price,state=location['state_code'],down_payment=float(.25 * offer_price),loan_amount=None,credit_score=700,has_mortgage=True)
+                        resp_json = address_lookup_service.roi_estimate(price=offer_price,rent=float(rent_price *12),state=location['state_code'],down_payment=float(.25 * offer_price),loan_amount=None,credit_score=700,has_mortgage=True)
                         if resp_json:
                             display_investment_summary(offer_price=offer_price, rent_price= rent_price, resp_json=resp_json)
                             st.write(f"Comparable sold recently")
